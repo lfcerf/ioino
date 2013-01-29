3aad264289527e2e96af09c63b874c2342992b5b
cedrick.faury@ac-clermont.fr <cedrick.faury@ac-clermont.fr@e4c36be1-ae91-2581-fe8a-76ff6401b9d7>
2013-01-29 15:41:23 +0000

    Ajout premier code Arduino SerialData

---
+++ trunk/Arduino/SerialData/SerialData.ino
@@ -0,0 +1,51 @@
+/*
+Protocole d'envoie de paquets de données
+lues sur un port analogique en temps réel
+*/
+
+// port lu
+int port = 0;
+
+// baudrate
+int baudrate = 11520;
+
+// taille du paquet
+int taillePaquet = 100;
+
+// paquet
+int temps[taillePaquet];
+int sortie[taillePaquet];
+
+// intervalle de prélèvement (us)
+int dt = 1000;
+
+// délai d'attente effectif (recalculé à chaque loop)
+int d = dt;
+
+void setup() {
+  Serial.begin(baudrate);  
+}
+
+void loop() {  
+  // Construction du paquet
+  for (int i=0;i<taillePaquet;i++){
+    if (i == 0){
+      delayMicroseconds(d);
+    }
+    else{
+      delayMicroseconds(dt);
+    }
+    temps[i] = micros();              // peut être faudra-t-il inverser ces 2 lignes ...
+    sortie[i] = analogRead(port);     //
+  }
+  // Ecriture du paquet sur le port série
+  Serial.print(temps);
+  Serial.print(" ");    // Caractère de séparation temps/sortie ... à voir ...
+  Serial.print(sortie);
+  
+  // Caractère de fin de paquet
+  Serial.println();
+  
+  // Calcul du prochain premier délai, (pour recoller le futur paquet au précédent)
+  d = dt - (micros()-temps[taillePaquet-1]);
+}
